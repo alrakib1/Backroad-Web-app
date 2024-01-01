b24d81d35e8618c5703d135086a0392a396b4ffd
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-01-01 12:57:34 +0600

    added services section

--- src/App.js
+++ src/App.js
@@ -1,61 +1,15 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
-     <Navbar/>
-
-      
-
-    
-
-      <section className="section services" id="services">
-        <div className="section-title">
-          <h2>
-            our <span>services</span>
-          </h2>
-        </div>
-        <div className="section-center services-center">
-          <article className="service">
-            <span className="service-icon">
-              <i className="fas fa-wallet fa-fw"></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">saving money</h4>
-              <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
-              </p>
-            </div>
-          </article>
-
-          <article className="service">
-            <span className="service-icon">
-              <i className="fas fa-tree fa-fw"></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">endless hiking</h4>
-              <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
-              </p>
-            </div>
-          </article>
-
-          <article className="service">
-            <span className="service-icon">
-              <i className="fas fa-socks fa-fw"></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">amazing comfort</h4>
-              <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
-              </p>
-            </div>
-          </article>
-        </div>
-      </section>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
 
       <section className="section" id="tours">
         <div className="section-title">
